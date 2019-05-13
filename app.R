fee3a0fae23515c711a91a3687a556ebc54ca9bb
Thiago Silva <tsranzanidasilva@gmail.com>
2019-05-13 17:06:55 -0300

    Modified Input

--- app.R
+++ app.R
@@ -13,12 +13,12 @@ ui <- fluidPage(
     # Sidebar panel for inputs ----
     sidebarPanel(
       
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      # Input: Slider for the number of nums ----
+      numericInput(inputId = "nums",
+                  label = "maHW:",
+                  min = 0,
+                  max = 10,
+                  value = 0.5)
       
     ),
     
@@ -34,20 +34,19 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
+  # Histogram ---
   # This expression that generates a histogram is wrapped in a call
   # to renderPlot to indicate that:
   #
   # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
+  #    re-executed when inputs (input$nums) change
   # 2. Its output type is a plot
   output$distPlot <- renderPlot({
     
     x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    nums <- seq(min(x), max(x), length.out = input$nums + 1)
     
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
+    hist(x, breaks = nums, col = "#75AADB", border = "white",
          xlab = "Waiting time to next eruption (in mins)",
          main = "Histogram of waiting times") 
     
