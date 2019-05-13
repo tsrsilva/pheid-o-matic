44fd38791e4cb758f18c90b1cae5cb1f10e22a8d
Thiago Silva <tsranzanidasilva@gmail.com>
2019-05-13 16:21:28 -0300

    Corrected app.

--- app/ui.r
+++ app/ui.r
@@ -6,5 +6,14 @@ ui <- fluidPage("Pheid-O-Matic",
                 sliderInput(inputId = "num",
                             label = "Add a value",
                             value = 5, min = 1, max = 100),
-                plotOutput("hist")
+                plotOutput("hist"),
+
+server <- function(input,output) {
+                output$hist <- renderPlot({
+                    title <- "5 random normal values"
+                    hist(rnorm(input$num), main =title)
+})
+}
+shinyApp(ui = ui, server = server)
+                
 )
