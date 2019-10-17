b51f9bfb8fc6dbd46fe22b43cd3a42b19f073c66
Thiago Silva <tsranzanidasilva@gmail.com>
2019-10-17 17:03:26 -0300

    Added r interaction with MySQL server

---
+++ conf_mysql.R
@@ -0,0 +1,43 @@
+#Install RMySQL to make R interact with the MySQL server
+install.packages('RMySQL')
+library('RMySQL')
+
+
+options(mysql = list(
+  "host" = "127.0.0.1",
+  "port" = 3306,
+  "user" = "myuser",
+  "password" = "mypassword"
+))
+databaseName <- "pheidole"
+table <- "responses"
+
+saveData <- function(data) {
+  # Connect to the database
+  db <- dbConnect(MySQL(), dbname = databaseName, host = options()$mysql$host, 
+                  port = options()$mysql$port, user = options()$mysql$user, 
+                  password = options()$mysql$password)
+  # Construct the update query by looping over the data fields
+  query <- sprintf(
+    "INSERT INTO %s (%s) VALUES ('%s')",
+    table, 
+    paste(names(data), collapse = ", "),
+    paste(data, collapse = "', '")
+  )
+  # Submit the update query and disconnect
+  dbGetQuery(db, query)
+  dbDisconnect(db)
+}
+
+loadData <- function() {
+  # Connect to the database
+  db <- dbConnect(MySQL(), dbname = databaseName, host = options()$mysql$host, 
+                  port = options()$mysql$port, user = options()$mysql$user, 
+                  password = options()$mysql$password)
+  # Construct the fetching query
+  query <- sprintf("SELECT * FROM %s", table)
+  # Submit the fetch query and disconnect
+  data <- dbGetQuery(db, query)
+  dbDisconnect(db)
+  data
+}
